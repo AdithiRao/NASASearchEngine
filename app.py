d14717115268a85be9494ea8d6ca5ed05dc5ab84
Adithi Rao <adigrao2012@gmail.com>
2019-03-23 20:41:39 -0400

    first

--- app.py
+++ app.py
@@ -5,9 +5,8 @@ from datetime import date
 
 app = Flask(__name__)
 
-@app.route('/')
-
 #Loads when the file is first loaded
+@app.route('/')
 def index():
     return render_template('index.html')
 
@@ -119,3 +118,20 @@ def getvalue():
 
 if __name__ == '__main__':
     app.run(debug=True)
+# from flask import Flask
+# from datetime import datetime
+# app = Flask(__name__)
+#
+# @app.route('/')
+# def homepage():
+#     the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
+#
+#     return """
+#     <h1>Hello heroku</h1>
+#     <p>It is currently {time}.</p>
+#
+#     <img src="http://loremflickr.com/600/400">
+#     """.format(time=the_time)
+#
+# if __name__ == '__main__':
+#     app.run(debug=True, use_reloader=True)
